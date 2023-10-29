92584f799daa2f59aae31bf162bf6649c23a6c21
Shin Paing Min <103930442+shinpaingmin@users.noreply.github.com>
2023-10-29 10:43:28 +0630

    Update db_config.php

--- db_connection/db_config.php
+++ db_connection/db_config.php
@@ -1,9 +1,9 @@
 <?php
 
 // Database configuration
-define('DB_HOST', 'localhost:3307');        // change your localhost name
-define('DB_USERNAME', 'shinpaingmin');      // change your username
-define('DB_PASSWORD', 'shinpaing');         // change your password
+define('DB_HOST', '');        // change your localhost name
+define('DB_USERNAME', '');      // change your username
+define('DB_PASSWORD', '');         // change your password
 define('DB_NAME', 'gwsc_db');               // change your db_name
 
 $conn = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
